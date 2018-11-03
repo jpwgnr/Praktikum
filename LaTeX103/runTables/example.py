9a093f83cec3bb474ac7780a98b0b09621a6ebc7
Lewis206 <jan.herdi@gmail.com>
2018-11-03 01:25:30 +0100

    update

--- LaTeX103/runTables/example.py
+++ LaTeX103/runTables/example.py
@@ -9,7 +9,7 @@ tabD1.writeFile('../tables/tableD1.tex')
 
 x2, D2a,D2b=np.genfromtxt("D2.txt", unpack=True)
 
-tabD2 = TexTable([D2a,D2b,x2], [r'$D(x)/mm$ ohne Gewicht', r'$D(x)/mm$ mit Gewicht', r'$x/cm'])
+tabD2 = TexTable([D2a,D2b,x2], [r'$D(x)/mm$ ohne Gewicht', r'$D(x)/mm$ mit Gewicht', r'$x/cm$'])
 
 tabD2.writeFile('../tables/tableD2.tex')
 
@@ -30,24 +30,26 @@ tabD3b.writeFile('../tables/tableD3b.tex')
 newD1, newx1=np.genfromtxt("newDnewX1.txt", unpack=True)
 
 tabnew1 = TexTable([newD1,newx1], ['$D(x)/m$ Differenz', r'$Lx^2-x^3/3 /m^3$'])
-
+tabnew1.setRowRounding(5, 3)
 tabnew1.writeFile('../tables/tablenew1.tex')
 
 
 newD2, newx2=np.genfromtxt("newDnewX2.txt", unpack=True)
-
 tabnew2 = TexTable([newD2,newx2], [r'$D(x)/m$ Differenz', r'$Lx^2-x^3/3 /m^3$'])
 
+tabnew2.setRowRounding(5, 3)
 tabnew2.writeFile('../tables/tablenew2.tex')
 
 newD3a, newx3a=np.genfromtxt("newDnewX3a.txt", unpack=True)
 
 tabnew3a = TexTable([newD3a,newx3a], [r'$D(x)/m$ Differenz', r'$3L^2x-4x^3 /m^3$'])
 
+tabnew3a.setRowRounding(5, 3)
 tabnew3a.writeFile('../tables/tablenew3a.tex')
 
 newD3b, newx3b=np.genfromtxt("newDnewX3b.txt", unpack=True)
 
 tabnew3b = TexTable([newD3b,newx3b], [r'$D(x)/m$ Differenz', r'$4x^3-12Lx^2+9L^2x-L^3 /^3m$'])
 
+tabnew3b.setRowRounding(5, 3)
 tabnew3b.writeFile('../tables/tablenew3b.tex')
