b945fac70c9afe0400768bc2d148f0b5f495f1dc
Lewis206 <jan.herdi@gmail.com>
2018-11-10 15:21:01 +0100

    new structure

--- Vorlage Protokoll/data/newtable.py
+++
@@ -1,9 +0,0 @@
-from table import TexTable
-import numpy as np 
-
-G1, L1,d1=np.genfromtxt("Stab1.txt", unpack=True)
-
-Stab1 = TexTable([G1, L1, d1], [r'Gewicht$/\si{\gram}$', r'Länge$/\si{\centi\meter}$', 'Durchmesser$/\si{\milli\meter}$'])
-
-Stab1.writeFile('../tables/Stab1.tex')
-
