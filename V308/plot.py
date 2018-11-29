872e2d648062b4364fa6d787c282590f056ae1df
Lewis206 <jan.herdi@gmail.com>
2018-11-29 22:29:36 +0100

    update

--- V308/plot.py
+++ V308/plot.py
@@ -39,23 +39,34 @@ tabc.writeFile("build/tabc.tex")
 #a)
 #lang
 n1lang=300
-l1lang=24e-2
+l1lang=<++>
 mu_rlang=1
 mu_0=4*np.pi*1e-7 
 Ilang= 1
+explangBmax= <++>
+
 #kurz
-n1kurz=300
-l1kurz=24e-2
+n1kurz=<++>
+l1kurz=<++>
 mu_rkurz=1
 Ikurz= 1
+expkurzmax=<++>
+expkurzBmax= <++>
 
 #b) 
-R2=6.25e-2
+R2=<++>
 n2=100
 d21=3.125e-2
 d22=6.5e-2 
 I21=4
 I23=3
+exphelmB1=<++>
+exphelmx1=<++>
+exphelmB2=<++>
+exphelmlinks=<++>
+exphelmB3=<++>
+exphelmrechts=<++>
+
 #c)
 Spalt= 3e-3 #mm
 n3= 595 
@@ -92,7 +103,8 @@ theoBlang= getBlong(1, mu_0, n1lang, l1lang, Ilang)
 
 #parameterslang, pcovlang = curve_fit(getBlong, x1lang, B1lang)
 
-theoBkurz= getBlong(1, mu_0, n1kurz, l1kurz, Ikurz)
+theox1kurz= np.linspace(x1kurz[0], x1kurz[-1], 200)
+theoBkurz= allgB(theox1kurz, mu_0, Ikurz, Rkurz, n1kurz, expkurzmax)
 
 parameterskurz, pcovkurz = curve_fit(allgB, x1kurz*1e2, B1kurz*1e3)
 
@@ -102,7 +114,15 @@ theoBhelmmitte1 = getBhelm(mu_0, I21,n2, R2,d21)
 theoBhelmmitte2 = getBhelm(mu_0, I21,n2, R2,d22)
 theoBhelmmitte3 = getBhelm(mu_0, I23,n2, R2,d22)
 
-#parametersHelm1, pcovHelm1 = curve_fit(allgB, x2r, B2r)
+theohelmx2r=np.linspace(x2r[0], x2r[-1], 200)
+theohelmx2d=np.linspace(x2d[0], x2d[-1], 200)
+theohelmx23= np.linspace(x23[0],x23[-1], 200)
+
+theoBhelm1= allgB(theohelmx2r, mu_0, I21, R2, n2, exphelm1)
+theoBhelm2= allgB2(theohelmx2d, mu_0, I21, R2, n2, exphelmlinks, exphelmrechts)
+theoBhelm3= allgB2(theohelmx23, mu_0, I23, R2, n2, exphelmlinks, exphelmrechts)
+
+parametersHelm1, pcovHelm1 = curve_fit(allgB, x2r, B2r)
 
 parametersHelm2, pcovHelm2 = curve_fit(allgB2, x2d*1e2, B2d*1e3)
 
@@ -112,9 +132,9 @@ parametersHelm3, pcovHelm3 = curve_fit(allgB2, x23*1e2, B23*1e3)
 #parametersTorus, pcovTorus = curve_fit(getBtorus, I3, B3)
 #abgelesene Ergebnisse: 
 
-SaetMag= 1
-Rema= 1
-Koerz= 1
+SaetMag= <++> 
+Rema= <++>
+Koerz= <++>
 
 #allgemeine Rechnungen
 #Steigung1, yAbschnitt1, r_value1, p_value1, std_err1= stats.linregress(x,y)
@@ -123,16 +143,16 @@ Koerz= 1
 
 #save solution 
 
-#file = open("build/solution.txt", "w")
-#file.write("a) \n\tExperimental:\n\t\tLange Spule B= {} T\n\t\tKurze Spule B max= {} T\n\n\tTheoretisch:\n\t\tLange Spule B= {} T\n\t\tKurze Spule B max= {} T\n\nb)\n\tExperimental:\n\t\tMittePaar r B= {} T\n\t\tMittePaar d B= {} T\n\t\tMitte Paar d2 B= {} T\n\n\t Theorie:\n\tTheorie:\n\t\tMitte Paar r B= {} T\n\t\t Mitte Paar d B= {} T\n\t\tMitte Paar d2 B= {} T\n\nc)\n\t Ergebnis aus Graph:\n\t\tSättigungsmagnetisierung{}\n\t\tRemanenz: {} T\n\t\tStromstärke bei Koerzitivkraft= {} A".format())
-#file.close()
+file = open("build/solution.txt", "w")
+file.write("a) \n\tExperimental:\n\t\tLange Spule B= {} T\n\t\tKurze Spule B max= {} T\n\n\tTheoretisch:\n\t\tLange Spule B= {} T\n\t\tKurze Spule B max= {} T\n\nb)\n\tExperimental:\n\t\tMittePaar r B= {} T\n\t\tMittePaar d B= {} T\n\t\tMitte Paar d2 B= {} T\n\n\t Theorie:\n\tTheorie:\n\t\tMitte Paar r B= {} T\n\t\t Mitte Paar d B= {} T\n\t\tMitte Paar d2 B= {} T\n\nc)\n\t Ergebnis aus Graph:\n\t\tSättigungsmagnetisierung= {} T\n\t\tRemanenz= {} T\n\t\tStromstärke bei Koerzitivkraft= {} A".format(explangBmax, expkurzBmax, theoBlang, theoBkurz, exphelmB1, exphelmB2, exphelmB3, theoBhelmmitte1, theoBhelmmitte2, theoBhelmmitte3, SaetMag, Rema, Koerz ))
+file.close()
 
 #Make plots for data
 #a)
 plt.figure(1)
 plt.plot(x1lang*1e2, B1lang*1e3, "xr", label="Daten")
 #plt.plot(t, f(t, *parameters), 'b-', label='Fit')
-#plt.plot(x1, function(<++>), "r", label="Fit", linewidth=1.0)
+plt.plot(x1lang*1e2, theoBlang*1e3, "r", label="Fit", linewidth=1.0)
 plt.xlabel(r"$x/\si{\centi\meter}$")
 plt.ylabel(r"$B/\si{\milli\tesla}$")
 plt.legend(loc="best")
@@ -141,8 +161,8 @@ plt.savefig("build/plota1.pdf")
 
 plt.figure(2)
 plt.plot(x1kurz*1e2, B1kurz*1e3, "xr", label="Daten")
-plt.plot(x1kurz*1e2, allgB(x1kurz*1e2, *parameterskurz), 'b-', label='Fit')
-#plt.plot(x1, function(<++>), "r", label="Fit", linewidth=1.0)
+plt.plot(theox1kurz*1e2, allgB(theox1kurz*1e2, *parameterskurz), 'b-', label='Fit')
+plt.plot(theox1kurz*1e2, theoBkurz*1e3, "r", label="Theoriekurve", linewidth=1.0)
 plt.xlabel(r"$x/\si{\centi\meter}$")
 plt.ylabel(r"$B/\si{\milli\tesla}$")
 plt.legend(loc="best")
@@ -152,30 +172,28 @@ plt.savefig("build/plota2.pdf")
 #b)
 plt.figure(3)
 plt.plot(x2r*1e2, B2r*1e3, "xr", label="Daten")
-#plt.plot(t, allgB(t, *parameterskurz), 'b-', label='Fit')
-#plt.plot(<++>, function(<++>), "r", label="Fit", linewidth=1.0)
+plt.plot(theohelmx2r*1e2, allgB(x2r*1e2, *parametersHelm1), 'b-', label='Fit')
+plt.plot(theohelmx2r*1e2, theoBhelm1, "r", label="Theoriekurve", linewidth=1.0)
 plt.xlabel(r"$x/\si{\centi\meter}$")
 plt.ylabel(r"$B/\si{\milli\tesla}$")
 plt.legend(loc="best")
 plt.tight_layout()
 plt.savefig("build/plotb1.pdf")
 
-x2dnew= np.linspace(x2d[0],x2d[-1], 200)
 plt.figure(4)
 plt.plot(x2d*1e2, B2d*1e3, "xr", label="Daten")
-plt.plot(x2dnew*1e2, allgB2(x2dnew*1e2, *parametersHelm2), 'b-', label='Fit')
-#plt.plot(<++>, function(<++>), "r", label="Fit", linewidth=1.0)
+plt.plot(theohelmx2d*1e2, allgB2(theohelmx2d*1e2, *parametersHelm2), 'b-', label='Fit')
+plt.plot(theohelmx2d*1e2, theoBhelm2, "r", label="Theoriekurve", linewidth=1.0)
 plt.xlabel(r"$x/\si{\centi\meter}$")
 plt.ylabel(r"$B/\si{\milli\tesla}$")
 plt.legend(loc="best")
 plt.tight_layout()
 plt.savefig("build/plotb2.pdf")
 
-x23new= np.linspace(x23[0],x23[-1], 200)
 plt.figure(5)
 plt.plot(x23*1e2, B23*1e3, "xr", label="Daten")
-plt.plot(x23new*1e2, allgB2(x23new*1e2, *parametersHelm3), 'b-', label='Fit')
-#plt.plot(<++>, function(<++>), "r", label="Fit", linewidth=1.0)
+plt.plot(theohelmx23*1e2, allgB2(theohelmx23*1e2, *parametersHelm3), 'b-', label='Fit')
+plt.plot(theohelmx23*1e2, theoBhelm3, "r", label="Theoriekurve", linewidth=1.0)
 plt.xlabel(r"$x/\si{\centi\meter}$")
 plt.ylabel(r"$B/\si{\milli\tesla}$")
 plt.legend(loc="best")
@@ -184,25 +202,12 @@ plt.savefig("build/plotb3.pdf")
 
 #c)
 plt.figure(6)
-plt.plot(I3, B3*1e3, "xr", label="Daten")
-#plt.plot(t, f(t, *parameters), 'b-', label='Fit')
-#plt.plot(<++>, function(<++>), "r", label="Fit", linewidth=1.0)
+plt.plot(I3[11:-1], B3[11:-1]*1e3, "r-", label="Hysteresekurve")
+plt.plot(I3[0:10], B3[0:10]*1e3, 'b-', label='Neukurve')
 plt.xlabel(r"$I/\si{\ampere}$")
 plt.ylabel(r"$B/\si{\milli\tesla}$")
+plt.Axes.set_axis_on()
 plt.legend(loc="best")
 plt.tight_layout()
 plt.savefig("build/plotc.pdf")
-
-#curvefit plot
-
-#plt.errorbar(x, y, yerr=err_y, fmt='rx', label='Daten')
-#t = np.linspace(-0.5, 2 * np.pi + 0.5)
-#plt.plot(t, f(t, *parameters), 'b-', label='Fit')
-#plt.plot(t, np.sin(t), 'g--', label='Original')
-#plt.xlim(t[0], t[-1])
-#plt.xlabel(r'$t$')
-#plt.ylabel(r'$f(t)$')
-#plt.legend(loc='best')
-#plt.tight_layout()
-#plt.savefig("build/plot1b.pdf")
 
