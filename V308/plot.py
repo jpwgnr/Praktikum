26722a4648a97748569a430ddb5188e035946022
lewis206 <jan.herdi@gmail.com>
2018-11-30 14:34:03 +0100

    update

--- V308/plot.py
+++ V308/plot.py
@@ -39,33 +39,34 @@ tabc.writeFile("build/tabc.tex")
 #a)
 #lang
 n1lang=300
-l1lang=<++>
+l1lang=16.0e-2
 mu_rlang=1
 mu_0=4*np.pi*1e-7 
 Ilang= 1
-explangBmax= <++>
+explangBmax= 2.24e-3
 
 #kurz
-n1kurz=<++>
-l1kurz=<++>
+n1kurz=100
+l1kurz=5.5e-2
 mu_rkurz=1
 Ikurz= 1
-expkurzmax=<++>
-expkurzBmax= <++>
+Rkurz= 2.05e-2
+expkurzmax=5.0e-2
+expkurzBmax= 1.8e-3
 
 #b) 
-R2=<++>
+R2=12.5e-2
 n2=100
 d21=3.125e-2
 d22=6.5e-2 
 I21=4
 I23=3
-exphelmB1=<++>
-exphelmx1=<++>
-exphelmB2=<++>
-exphelmlinks=<++>
-exphelmB3=<++>
-exphelmrechts=<++>
+exphelmB1=5.6e-3
+exphelmx1=1.0e-2
+exphelmB2=2.5e-3
+exphelmB3=2.25e-3
+exphelmrechts=9.5e-2
+exphelmlinks=exphelmrechts-d22
 
 #c)
 Spalt= 3e-3 #mm
@@ -85,7 +86,7 @@ def getBlong(mu_r, mu_0, n, l, I):
 
 #b)
 def getBhelm(mu_0, I, n, R, x):
-    return 2*n*mu_0*I*(R**2)/((R**2+x**2)**(3/2))
+    return n*mu_0*I*(R**2)/((R**2+x**2)**(3/2))
 
 def getBhelmgrad(mu_0, I, n, R, x):
     return (-3*mu_0*I*R**2*x)/((R**2+x**2)**(5/2))
@@ -105,8 +106,9 @@ theoBlang= getBlong(1, mu_0, n1lang, l1lang, Ilang)
 
 theox1kurz= np.linspace(x1kurz[0], x1kurz[-1], 200)
 theoBkurz= allgB(theox1kurz, mu_0, Ikurz, Rkurz, n1kurz, expkurzmax)
-
+theoBkurzmax= max(theoBkurz)
 parameterskurz, pcovkurz = curve_fit(allgB, x1kurz*1e2, B1kurz*1e3)
+
 
 #b) 
 
@@ -114,27 +116,25 @@ theoBhelmmitte1 = getBhelm(mu_0, I21,n2, R2,d21)
 theoBhelmmitte2 = getBhelm(mu_0, I21,n2, R2,d22)
 theoBhelmmitte3 = getBhelm(mu_0, I23,n2, R2,d22)
 
-theohelmx2r=np.linspace(x2r[0], x2r[-1], 200)
-theohelmx2d=np.linspace(x2d[0], x2d[-1], 200)
-theohelmx23= np.linspace(x23[0],x23[-1], 200)
+theohelmx2r=np.linspace(x2r[0]-14e-2, x2r[-1]+5e-2, 200)
+theohelmx2d=np.linspace(x2d[0]-14e-2, x2d[-1]+5e-2, 200)
+theohelmx23= np.linspace(x23[0]-14e-2,x23[-1]+5e-2, 200)
 
-theoBhelm1= allgB(theohelmx2r, mu_0, I21, R2, n2, exphelm1)
+#theoBhelm1= allgB(theohelmx2r, mu_0, I21, R2, n2, exphelmx1)
 theoBhelm2= allgB2(theohelmx2d, mu_0, I21, R2, n2, exphelmlinks, exphelmrechts)
 theoBhelm3= allgB2(theohelmx23, mu_0, I23, R2, n2, exphelmlinks, exphelmrechts)
 
-parametersHelm1, pcovHelm1 = curve_fit(allgB, x2r, B2r)
-
+parametersHelm1, pcovHelm1 = curve_fit(allgB, x2r*1e2, B2r*1e3)
 parametersHelm2, pcovHelm2 = curve_fit(allgB2, x2d*1e2, B2d*1e3)
-
 parametersHelm3, pcovHelm3 = curve_fit(allgB2, x23*1e2, B23*1e3)
 
 #c)
 #parametersTorus, pcovTorus = curve_fit(getBtorus, I3, B3)
 #abgelesene Ergebnisse: 
 
-SaetMag= <++> 
-Rema= <++>
-Koerz= <++>
+SaetMag= 704.3e-3 
+Rema= 122.8e-3
+Koerz= -0.5
 
 #allgemeine Rechnungen
 #Steigung1, yAbschnitt1, r_value1, p_value1, std_err1= stats.linregress(x,y)
@@ -144,7 +144,7 @@ Koerz= <++>
 #save solution 
 
 file = open("build/solution.txt", "w")
-file.write("a) \n\tExperimental:\n\t\tLange Spule B= {} T\n\t\tKurze Spule B max= {} T\n\n\tTheoretisch:\n\t\tLange Spule B= {} T\n\t\tKurze Spule B max= {} T\n\nb)\n\tExperimental:\n\t\tMittePaar r B= {} T\n\t\tMittePaar d B= {} T\n\t\tMitte Paar d2 B= {} T\n\n\t Theorie:\n\tTheorie:\n\t\tMitte Paar r B= {} T\n\t\t Mitte Paar d B= {} T\n\t\tMitte Paar d2 B= {} T\n\nc)\n\t Ergebnis aus Graph:\n\t\tSättigungsmagnetisierung= {} T\n\t\tRemanenz= {} T\n\t\tStromstärke bei Koerzitivkraft= {} A".format(explangBmax, expkurzBmax, theoBlang, theoBkurz, exphelmB1, exphelmB2, exphelmB3, theoBhelmmitte1, theoBhelmmitte2, theoBhelmmitte3, SaetMag, Rema, Koerz ))
+file.write("a) \n\tExperimental:\n\t\tLange Spule B= {} T\n\t\tKurze Spule B max= {} T\n\n\tTheoretisch:\n\t\tLange Spule B= {} T\n\t\tKurze Spule B max= {} T\n\nb)\n\tExperimental:\n\t\tMittePaar r B= {} T\n\t\tMittePaar d B= {} T\n\t\tMitte Paar d2 B= {} T\n\n\tTheorie:\n\t\tMitte Paar r B= {} T\n\t\tMitte Paar d B= {} T\n\t\tMitte Paar d2 B= {} T\n\nc)\n\t Ergebnis aus Graph:\n\t\tSättigungsmagnetisierung= {} T\n\t\tRemanenz= {} T\n\t\tStromstärke bei Koerzitivkraft= {} A".format(explangBmax, expkurzBmax, theoBlang, theoBkurzmax, exphelmB1, exphelmB2, exphelmB3, theoBhelmmitte1, theoBhelmmitte2, theoBhelmmitte3, SaetMag, Rema, Koerz ))
 file.close()
 
 #Make plots for data
@@ -152,9 +152,12 @@ file.close()
 plt.figure(1)
 plt.plot(x1lang*1e2, B1lang*1e3, "xr", label="Daten")
 #plt.plot(t, f(t, *parameters), 'b-', label='Fit')
-plt.plot(x1lang*1e2, theoBlang*1e3, "r", label="Fit", linewidth=1.0)
+#plt.plot(x1lang*1e2, theoBlang*1e3, "r", label="Fit", linewidth=1.0)
 plt.xlabel(r"$x/\si{\centi\meter}$")
 plt.ylabel(r"$B/\si{\milli\tesla}$")
+plt.grid()
+plt.axhline(y=0, color="k")
+plt.axvline(x=0, color="k")
 plt.legend(loc="best")
 plt.tight_layout()
 plt.savefig("build/plota1.pdf")
@@ -162,9 +165,12 @@ plt.savefig("build/plota1.pdf")
 plt.figure(2)
 plt.plot(x1kurz*1e2, B1kurz*1e3, "xr", label="Daten")
 plt.plot(theox1kurz*1e2, allgB(theox1kurz*1e2, *parameterskurz), 'b-', label='Fit')
-plt.plot(theox1kurz*1e2, theoBkurz*1e3, "r", label="Theoriekurve", linewidth=1.0)
+#plt.plot(theox1kurz*1e2, theoBkurz*1e3, "r", label="Theoriekurve", linewidth=1.0)
 plt.xlabel(r"$x/\si{\centi\meter}$")
 plt.ylabel(r"$B/\si{\milli\tesla}$")
+plt.grid()
+plt.axhline(y=0, color="k")
+plt.axvline(x=0, color="k")
 plt.legend(loc="best")
 plt.tight_layout()
 plt.savefig("build/plota2.pdf")
@@ -172,10 +178,13 @@ plt.savefig("build/plota2.pdf")
 #b)
 plt.figure(3)
 plt.plot(x2r*1e2, B2r*1e3, "xr", label="Daten")
-plt.plot(theohelmx2r*1e2, allgB(x2r*1e2, *parametersHelm1), 'b-', label='Fit')
-plt.plot(theohelmx2r*1e2, theoBhelm1, "r", label="Theoriekurve", linewidth=1.0)
+plt.plot(theohelmx2r*1e2, allgB(theohelmx2r*1e2, *parametersHelm1), 'b-', label='Fit')
+#plt.plot(theohelmx2r*1e2, theoBhelm1*1e3, "r", label="Theoriekurve", linewidth=1.0)
 plt.xlabel(r"$x/\si{\centi\meter}$")
 plt.ylabel(r"$B/\si{\milli\tesla}$")
+plt.grid()
+plt.axhline(y=0, color="k")
+plt.axvline(x=0, color="k")
 plt.legend(loc="best")
 plt.tight_layout()
 plt.savefig("build/plotb1.pdf")
@@ -183,9 +192,12 @@ plt.savefig("build/plotb1.pdf")
 plt.figure(4)
 plt.plot(x2d*1e2, B2d*1e3, "xr", label="Daten")
 plt.plot(theohelmx2d*1e2, allgB2(theohelmx2d*1e2, *parametersHelm2), 'b-', label='Fit')
-plt.plot(theohelmx2d*1e2, theoBhelm2, "r", label="Theoriekurve", linewidth=1.0)
+#plt.plot(theohelmx2d*1e2, theoBhelm2*1e3, "r", label="Theoriekurve", linewidth=1.0)
 plt.xlabel(r"$x/\si{\centi\meter}$")
 plt.ylabel(r"$B/\si{\milli\tesla}$")
+plt.grid()
+plt.axhline(y=0, color="k")
+plt.axvline(x=0, color="k")
 plt.legend(loc="best")
 plt.tight_layout()
 plt.savefig("build/plotb2.pdf")
@@ -193,9 +205,12 @@ plt.savefig("build/plotb2.pdf")
 plt.figure(5)
 plt.plot(x23*1e2, B23*1e3, "xr", label="Daten")
 plt.plot(theohelmx23*1e2, allgB2(theohelmx23*1e2, *parametersHelm3), 'b-', label='Fit')
-plt.plot(theohelmx23*1e2, theoBhelm3, "r", label="Theoriekurve", linewidth=1.0)
+#plt.plot(theohelmx23*1e2, theoBhelm3*1e3, "r", label="Theoriekurve", linewidth=1.0)
 plt.xlabel(r"$x/\si{\centi\meter}$")
 plt.ylabel(r"$B/\si{\milli\tesla}$")
+plt.grid()
+plt.axhline(y=0, color="k")
+plt.axvline(x=0, color="k")
 plt.legend(loc="best")
 plt.tight_layout()
 plt.savefig("build/plotb3.pdf")
@@ -206,7 +221,9 @@ plt.plot(I3[11:-1], B3[11:-1]*1e3, "r-", label="Hysteresekurve")
 plt.plot(I3[0:10], B3[0:10]*1e3, 'b-', label='Neukurve')
 plt.xlabel(r"$I/\si{\ampere}$")
 plt.ylabel(r"$B/\si{\milli\tesla}$")
-plt.Axes.set_axis_on()
+plt.grid()
+plt.axhline(y=0, color="k")
+plt.axvline(x=0, color="k")
 plt.legend(loc="best")
 plt.tight_layout()
 plt.savefig("build/plotc.pdf")
