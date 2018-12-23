0418a06fdac8054dc2696dff54c6212d36598340
Lewis206 <jan.herdi@gmail.com>
2018-12-23 09:20:57 +0100

    update

--- V207/plot.py
+++ V207/plot.py
@@ -10,17 +10,17 @@ from math import *
 
 #from txt
 t1small, t1big = np.genfromtxt("data/dataa.txt", unpack=True)
-tab1 = TexTable([t1small,t1big], [r"$t_\text{klein} /\si{\second}$", r"$t_\text{groß} /\si{\second}$"], label="tab1", caption="Die Falldauer der kleinen Kugel und die Falldauer der großen Kugel.", roundPrecision=1)
+tab1 = TexTable([t1small,t1big], [r"$t_\text{klein} /\si{\second}$", r"$t_\text{groß} /\si{\second}$"], label="tab1", caption= r"Die Falldauer der kleinen Kugel und die Falldauer der großen Kugel.", roundPrecision=1)
 tab1.writeFile("build/tab1.tex")
 
 T11, t1 = np.genfromtxt("data/datab.txt", unpack=True)
 T1= T11+273.15
-tab2 = TexTable([T1,t1], [r"T /\si{\kelvin}", r"t /\si{\second}"], label="tab2", caption="Die Fallzeit in Abhängigkeit zur Temperatur der Flüssigkeit.", roundPrecision=1)
+tab2 = TexTable([T1,t1], [r"$T_1 /\si{\kelvin}$", r"$t /\si{\second}$"], label="tab2", caption= r"Die Fallzeit in Abhängigkeit zur Temperatur der Flüssigkeit.", roundPrecision=1)
 tab2.writeFile("build/tab2.tex")
 
 T21, t2 = np.genfromtxt("data/datac.txt", unpack=True)
 T2= T21+273.15
-tab3 = TexTable([T2,t2], [r"T/\si{\kelvin}", r"t /\si{\second}"], label="tab3", caption="Die Fallzeit in Abhängigkeit zur Temperatur der Flüssigkeit.", roundPrecision=1)
+tab3 = TexTable([T2,t2], [r"$T_2 /\si{\kelvin}$", r"$t /\si{\second}$"], label="tab3", caption= r"Die Fallzeit in Abhängigkeit zur Temperatur der Flüssigkeit.", roundPrecision=1)
 tab3.writeFile("build/tab3.tex")
 #extra values 
 #a)
@@ -65,6 +65,8 @@ vwater2= x/tbig
 #Steigung1, yAbschnitt1, r_value1, p_value1, std_err1= stats.linregress(x,y)
 params1, pcov1 = curve_fit(getEta, 1/T1,np.log(eta1))
 params2, pcov2 = curve_fit(getEta, 1/T2,np.log(eta2) )
+paramserr1= np.sqrt(np.diag(pcov1))
+paramserr2= np.sqrt(np.diag(pcov2))
 
 newT1= np.linspace(T1[0], T1[-1], 200)
 newT2= np.linspace(T2[0], T2[-1], 200)
@@ -76,8 +78,17 @@ rey2=(rhoFl*dbig*vwater2)/eta
 
 #save solution 
 
+tab4 = TexTable([eta1,eta2], [r"$\eta_1 /\si{\pascal\second}$", r"$\eta_2 /\si{\pascal\second}$"], label="tab4", caption= r"Die Viskosität für die erste und zweite Messung.", roundPrecision=1)
+tab4.writeFile("build/tab4.tex")
+
+tab5 = TexTable([1/T1, np.log(eta1)], [r"$1/T_1 /\si[per-mode=fraction]{\per\kelvin}$", r"$\eta_1 /\si{\pascal\second}$"], label="tab5", caption= r"Die invertierte Temperatur gegen die logarithmierte Viskosität für die erste Messung.", roundPrecision=1)
+tab5.writeFile("build/tab5.tex")
+
+tab6 = TexTable([1/T2, np.log(eta2)], [r"$1/T_2 /\si[per-mode=fraction]{\per\kelvin}$", r"$\eta_2 /\si{\pascal\second}$"], label="tab6", caption= r"Die invertierte Temperatur gegen die logarithmierte Viskosität für die zweite Messung.", roundPrecision=1)
+tab6.writeFile("build/tab6.tex")
+
 file = open("build/solution.txt", "w")
-file.write("Dichte_klein= {}\nDichte_groß= {}\nDichte_Fl= {}\nA1= {}\nB1= {}\nA2= {}\nB2= {}\nReynoldsche Zahl Kugel_small= {}\nReynoldsche Zahl Kugel_big= {}".format(rhosmall, rhobig, rhoFl, params1[0], params1[1], params2[0], params2[1], rey1, rey2))
+file.write("Dichte_klein= {} kg/m^3\nDichte_groß= {} kg/m^3\nDichte_Fl= {} kg/m^3\nDurchmesser_klein= {} m\nDurchmesser_groß= {} m\nt_klein= {} s\nt_groß= {} s\nK_klein= {} Pa*m^3/kg\nK_groß= {} Pa*m^3/kg\nEta_klein= {} Pa*s\nA1= {} +/- {} Pa*s\nB1= {} +/- {} K\nA2= {} +/- {} Pa*s\nB2= {} +/- {} K\nv_klein= {} m/s\nv_groß= {} m/s\nReynoldsche Zahl Kugel_small= {}\nReynoldsche Zahl Kugel_big= {}".format(rhosmall, rhobig, rhoFl, dsmall, dbig, tsmall, tbig, Ksmall, Kbig, eta, params1[0], paramserr1[0], params1[1], paramserr1[1], params2[0], paramserr2[0], params2[1], paramserr2[1], vwater1, vwater2, rey1, rey2))
 file.close()
 
 #Make plots for data
