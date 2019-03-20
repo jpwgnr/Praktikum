1b5b3565ccf9a1bcce42e0d9220144ba9803ccf2
Lewis206 <jan.herdi@gmail.com>
2019-03-20 16:55:11 +0100

    update

--- Vorlage Protokoll/plot.py
+++ Vorlage Protokoll/plot.py
@@ -1,59 +1,26 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import uncertainties.unumpy as unp
-from table import TexTable
 from scipy import stats
 from scipy.optimize import curve_fit
+from something import some 
 #Generate data 
-
-#from txt
-<++>, <++> = np.genfromtxt("data/data<++>.txt", unpack=True)
-tab<++> = TexTable([<++>,<++>], [r"<++>", r"<++>"], label="tab<++>", caption="<++>", roundPrecision=<++>)
-tab<++>.writeFile("build/tab<++>.tex")
-
-#extra values 
-a=0
+<++>= some.neueWerte(file_name="data/data<++>.txt", finished_file="build/tab<++>.tex",  vars_name=[r"<++>", r"<++>"], label_text="tab<++>", caption_text=r"<++>." , precision=<++>)
+#Generate table with calculated data
+some.tabelle(vars, finished_file="tab<++>.tex", vars_name=[r"<++>", r"<++>"], label_text="tab<++>", caption_text=r"<++>", precision=2) 
+#extra values
 
 #functions 
 
-def func(x):
-    return x+1
+#Generate linReg-Plot
+some.linReg(x=<++>, y=<++>, x_name=r"<++>", y_name=r"<++>", num=<++>,  x_add=<++>, file_name="build/plot<++>.pdf")
+#Generate curve-fit-Plot 
+some.curvefit(x=<++>, y=<++>, num=<++>, x_add=<++>, function=<++>, x_name=r"<++>", y_name=r"<++>", file_name="build/plot<++>.pdf")
 
-def f(x, a, b, c, d):
-     return a * np.sin(b * x + c) + d
-#calculate 
-
-Steigung1, yAbschnitt1, r_value1, p_value1, std_err1= stats.linregress(x,y)
- 
-parameters, pcov = curve_fit(f, x, y, sigma=err_x)
-
-#save solution 
-
+#save solution
+Steigung1 = <++> 
+Fehler = <++>
 file = open("build/solution.txt", "w")
-file.write("Steigung = {} Fehler: {}".format(Steigung1, np.sqrt(np.diag(pcov)) ))
+file.write(f"Steigung = {Steigung1} Fehler: {Fehler}")
 file.close()
-
-#Make plots for data
-plt.figure(1)
-plt.plot(<++>, <++>, "xr", label="Daten")
-plt.plot(<++>, function(<++>), "r", label="Fit", linewidth=1.0)
-plt.xlim([0], x[-1])
-plt.xlabel(r"$t/\si{\second}$")
-plt.ylabel(r"$y(x)$")
-plt.legend(loc="best")
-plt.tight_layout()
-plt.savefig("build/plot1.pdf")
-
-#curvefit plot
-
-plt.errorbar(x, y, yerr=err_y, fmt='rx', label='Daten')
-t = np.linspace(-0.5, 2 * np.pi + 0.5)
-plt.plot(t, f(t, *parameters), 'b-', label='Fit')
-plt.plot(t, np.sin(t), 'g--', label='Original')
-plt.xlim(t[0], t[-1])
-plt.xlabel(r'$t$')
-plt.ylabel(r'$f(t)$')
-plt.legend(loc='best')
-plt.tight_layout()
-plt.savefig("build/plot1b.pdf")
 
