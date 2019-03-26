a63bd984d9d5432ae8a759377144c4f1039b4618
Lewis206 <jan.herdi@gmail.com>
2019-03-26 14:39:43 +0100

    update

--- MaxwellWheel/plot.py
+++ MaxwellWheel/plot.py
@@ -8,9 +8,11 @@ from scipy.optimize import curve_fit
 #Generate data 
 
 #from txt
+# Höhe und die Zeit für diese Strecke
 h1, t1= np.genfromtxt("data/dataa.txt", unpack=True)
 tab1 = TexTable([h1,t1], [r"$h1 \si{\centi\meter}$", r"$t \si{\second}$"], label="tab1", caption=r"Die Strecke $s$ aufgetragen gegen die Zeit $t$.", roundPrecision=2)
 tab1.writeFile("build/tab1.tex")
+#Umrechnung in Meter
 h1=h1/100
 
 h2, t2= np.genfromtxt("data/datab.txt", unpack=True)
@@ -23,35 +25,50 @@ tab3 = TexTable([h3,t3], [r"$h3 \si{\centi\meter}$", r"$t \si{\second}$"], label
 tab3.writeFile("build/tab3.tex")
 h3=h3/100
 
+#Höhe, Zeit und Umdrehungen im unteren Bereich
 h4, t4, umdreh= np.genfromtxt("data/datad.txt", unpack=True)
 tab4 = TexTable([h4,t4,umdreh], [r"$h4 \si{\centi\meter}$", r"$t \si{\second}$",r"Anzahl der Umdrehungen"], label="tab1", caption=r"Die Strecke $s$ aufgetragen gegen die Zeit $t$.", roundPrecision=2)
 tab4.writeFile("build/tab4.tex")
+
+#Höhe in Meter 
 h4=h4/100 -0.264 
+#Zeit Umgerechnet wegen der Frame Rate des Videos
 t4=t4*(5/4)
-T=t4/umdreh
+#Zeit pro Umdrehung
+T=t4/umdreh 
+#Winkelgeschwindigkeit des Rades 
 omega= 2*np.pi*(1/T)
 
 #extra values 
+#Radius des Zylinders
 R= 0.127/2
+#Radius von der Zylinderachse zur Schwerpunktsachse
 r= 0.006/2
+#Masse des Zylinders
 m= 0.43564  
+#Höhe über Boden
 tiefe= 0.264
 
+#Delta s, Länge des Bandes bei 10 Umdrehungen
 s=0.206
+#Berechneter Radius von Zylinderachse zu Schwerpunktsachse
 r_eff= s/(20*np.pi)
+#Trägheitsmoment Zylinder+ Steiner 
 I= 0.5*m*R**2+m*r_eff**2
 
 #functions 
-
+#Distance
 def dist(t,x,p,q):
    return 0.5*(x-q)*t**2+p 
 
+#Gerade
 def line(m,x,n):
     return m*x+n
 
 #calculate 
 
 #a) 
+#Länge der Strecke für die jeweilige Zeit
 newh1= np.empty(len(h1))
 for i in range(1,16):
     newh1[0]= 0
@@ -75,8 +92,10 @@ t1= np.delete(t1, 0)
 t2= np.delete(t2, 0)
 t3= np.delete(t3, 0)
 
+#Beschleunigung nach Formel
 a_theo= (1/(1+((R**2)/2*r_eff**2)))*9.81
 
+#Lineare Regression: x = t^2, y = höhe
 Steigung1, yAbschnitt1, r_value1, p_value1, std_err1= stats.linregress(t1**2, newh1)
 Steigung2, yAbschnitt2, r_value2, p_value2, std_err2= stats.linregress(t2**2, newh2)
 Steigung3, yAbschnitt3, r_value3, p_value3, std_err3= stats.linregress(t3**2, newh3)
@@ -84,7 +103,7 @@ params1, pcov1 = curve_fit(dist, t1, newh1)
 params2, pcov2 = curve_fit(dist, t2, newh2)
 params3, pcov3 = curve_fit(dist, t3, newh3)
 
-a_exp= np.array([Steigung1, Steigung2, Steigung3]) 
+a_exp= np.array([0.5*Steigung1, 0.5*Steigung2, 0.5*Steigung3]) 
 
 E_pot= m*9.81*h4 
 E_rot=1/2* I* omega**2 
@@ -96,6 +115,7 @@ file.write("Maxwell'sches Rad\n\na_exp= {} +- {}m/s^2\na_theo= {} m/s^2\n\nAbrol
 file.close()
 
 #curvefit plot
+#Plot Werte wie sie sind, Zeit gegen Höhe des Rades
 plt.figure(1)
 plt.errorbar(t1, newh1, fmt='rx', label='Daten')
 newt1= np.linspace(t1[0]+1, t1[-1]-1, 500)
@@ -126,6 +146,7 @@ plt.legend(loc='best')
 plt.tight_layout()
 plt.savefig("build/plot3a.pdf")
 
+#irrelevanter Plot
 plt.figure(8)
 plt.errorbar(index, newh3, fmt='rx', label='Daten')
 plt.ylabel(r'$s/ \si{\meter}$')
@@ -134,6 +155,7 @@ plt.legend(loc='best')
 plt.tight_layout()
 plt.savefig("build/plot5a.pdf")
 
+# Lineare Regression 
 plt.figure(4)
 plt.errorbar(t1**2, newh1, fmt='rx', label='Daten')
 plt.plot(newt1**2, line(Steigung1,newt1**2,yAbschnitt1), 'b-', label='Fit')
@@ -162,8 +184,9 @@ plt.legend(loc='best')
 plt.tight_layout()
 plt.savefig("build/plot3b.pdf")
 
+#Potentielle Energie und Rotationsenergie
 plt.figure(7)
-plt.plot(h4, E_pot, 'r-', label='potentielle Energie')
+plt.plot(h4, E_pot, 'r-', label='Potentielle Energie')
 plt.plot(h4, E_rot, 'b-', label='Rotationsenergie')
 plt.plot(h4, E_pot, 'rx')
 plt.plot(h4, E_rot, 'bx')
