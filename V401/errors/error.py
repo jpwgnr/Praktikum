4ccf8b36e4c396885dc852c8f07e24e1b773f88c
Lewis206 <jan.herdi@gmail.com>
2019-05-14 19:52:23 +0200

    update

--- V401/errors/error.py
+++ V401/errors/error.py
@@ -8,17 +8,17 @@ def error(f, err_vars=None):
         err_vars = f.free_symbols 
 
     for v in err_vars: 
-        if v != (a and b): #add here all the variables you want as constants. for more than 2 use an "and".  
+        if v != T0 and v !=p0 : #add here all the variables you want as constants. for more than 2 use an "and".  
             err = Symbol('latex_std_'+ v.name)
             s += f.diff(v)**2 * err**2 
             latex_names[err] = '\\sigma_{' + latex(v) + '}'
 
     return latex(sympy.sqrt(s), symbol_names=latex_names)
 
-a, b, E, q, r =sympy.var('a b E_x q r') #initalise all your variables at this point
+n, T, T0, p0, p =sympy.var('\Delta_n T T_0 p_0 \Delta_p') #initalise all your variables at this point
 
-f =a* E + q**2 *r +2*b**2  #input your function at this point
+f = 1 + n* T/T0 *p0/p  #input your function at this point
 
-file = open("error.txt", "w")
+file = open("fehler3.txt", "w")
 file.write("Formel: {}\n\nFehlerfortpflanzung: {}".format(f, error(f)))
 file.close()
