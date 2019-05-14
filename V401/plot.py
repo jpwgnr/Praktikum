03abbab4c773dbccedebec7906bf80fcb7454493
Lewis206 <jan.herdi@gmail.com>
2019-05-14 17:11:50 +0200

    update

--- V401/plot.py
+++ V401/plot.py
@@ -1,59 +1,44 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import uncertainties.unumpy as unp
-from table import TexTable
 from scipy import stats
+from uncertainties import ufloat
 from scipy.optimize import curve_fit
+from something import some 
 #Generate data 
+anz1, ds1, de1 = some.neueWerte(file_name="data/dataa.txt", finished_file="build/taba.tex",  vars_name=[r"Anzahl", r"$d_\text{Start} / \si{\milli\meter}$", r"$d_\text{Start} / \si{\milli\meter}$"], label_text="taba", caption_text=r"Die Anzahl der Impulse, der Startwert auf der Millimeterschraube und der Endwert auf der Millimeterschraube." , precision=2)
+anz2, deltap = some.neueWerte(file_name="data/datab.txt", finished_file="build/tabb.tex",  vars_name=[r"Anzahl", r"$\Delta p$"], label_text="tabb", caption_text=r"Die Anzahl der Impulse und der dazu gehörende $\Delta p= \SI{0.6}{\bar}$." , precision=2)
 
-#from txt
-<++>, <++> = np.genfromtxt("data/data<++>.txt", unpack=True)
-tab<++> = TexTable([<++>,<++>], [r"<++>", r"<++>"], label="tab<++>", caption="<++>", roundPrecision=<++>)
-tab<++>.writeFile("build/tab<++>.tex")
+ds1, de1 = ds1/1e3, de1/1e3 
 
-#extra values 
-a=0
+deltad1 = np.abs(ds1-de1)/5.017
+
+deltad1 = ufloat(deltad1.mean(), deltad1.std())
+
+anz1 = ufloat(anz1.mean(), anz1.std()) 
+anz2 = ufloat(anz2.mean(), anz2.std())
+
+b = 0.05 #m
+lambda1 = 2*deltad1/anz1
+deltan = anz2*lambda1/2 /b
+T0 = 293.15 
+T = 273.15
+p0 = 1.0132
+p = 0.6
+n = 1+ deltan * T0/T * p0/p
+#Generate table with calculated data
+#some.tabelle(vars, finished_file="tab<++>.tex", vars_name=[r"<++>", r"<++>"], label_text="tab<++>", caption_text=r"<++>", precision=2) 
+#extra values
 
 #functions 
 
-def func(x):
-    return x+1
+#Generate linReg-Plot
+#some.linReg(x=<++>, y=<++>, x_name=r"<++>", y_name=r"<++>", num=<++>,  x_add=<++>, file_name="build/plot<++>.pdf")
+#Generate curve-fit-Plot 
+#some.curvefit(x=<++>, y=<++>, num=<++>, x_add=<++>, function=<++>, x_name=r"<++>", y_name=r"<++>", file_name="build/plot<++>.pdf")
 
-def f(x, a, b, c, d):
-     return a * np.sin(b * x + c) + d
-#calculate 
-
-Steigung1, yAbschnitt1, r_value1, p_value1, std_err1= stats.linregress(x,y)
- 
-parameters, pcov = curve_fit(f, x, y, sigma=err_x)
-
-#save solution 
-
+#save solution
 file = open("build/solution.txt", "w")
-file.write("Steigung = {} Fehler: {}".format(Steigung1, np.sqrt(np.diag(pcov)) ))
+file.write(f"delta d 1 = {deltad1} m\nanz1 = {anz1}\nLambda = {lambda1} m \nanz2 = {anz2}\ndelta n = {deltan}\nn = {n}")
 file.close()
-
-#Make plots for data
-plt.figure(1)
-plt.plot(<++>, <++>, "xr", label="Daten")
-plt.plot(<++>, function(<++>), "r", label="Fit", linewidth=1.0)
-plt.xlim([0], x[-1])
-plt.xlabel(r"$t/\si{\second}$")
-plt.ylabel(r"$y(x)$")
-plt.legend(loc="best")
-plt.tight_layout()
-plt.savefig("build/plot1.pdf")
-
-#curvefit plot
-
-plt.errorbar(x, y, yerr=err_y, fmt='rx', label='Daten')
-t = np.linspace(-0.5, 2 * np.pi + 0.5)
-plt.plot(t, f(t, *parameters), 'b-', label='Fit')
-plt.plot(t, np.sin(t), 'g--', label='Original')
-plt.xlim(t[0], t[-1])
-plt.xlabel(r'$t$')
-plt.ylabel(r'$f(t)$')
-plt.legend(loc='best')
-plt.tight_layout()
-plt.savefig("build/plot1b.pdf")
 
