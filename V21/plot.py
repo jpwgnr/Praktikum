7a8cc4304ac574bc5f23ac98794bc94ee3c261e9
Lewis206 <jan.herdi@gmail.com>
2020-07-04 14:28:31 +0200

    erste Ergebnisse

--- V21/plot.py
+++ V21/plot.py
@@ -5,82 +5,61 @@ from scipy import stats
 from uncertainties import ufloat
 from scipy.optimize import curve_fit
 from something import some 
-#Generate data 
-p1, pulse1, max1= some.neueWerte(file_name="data/dataa.txt", finished_file="build/taba.tex",  vars_name=[r"$p / \si{\milli\bar}$", r"$\frac{N}{\SI{120}{\second}}$", r"$\text{Maximum Position}$"], label_text="taba", caption_text=r"Die Werte für den Druck in dem Glaszylinder, die Anzahl der Pulse und die Position des Maximums bei einem Abstand von $d_1 = \SI{2.7}{\centi\meter}$." , precision=1)
-p2, pulse2, max2= some.neueWerte(file_name="data/datab.txt", finished_file="build/tabb.tex",  vars_name=[r"$p / \si{\milli\bar}$", r"$\frac{N}{\SI{120}{\second}}$", r"$\text{Maximum Position}$"], label_text="tabb", caption_text=r"Die Werte für den Druck in dem Glaszylinder, die Anzahl der Pulse und die Position des Maximums bei einem Abstand $d = \SI{2}{\centi\meter}$." , precision=1)
-twelve, pulse3 = some.neueWerte(file_name="data/datac.txt", finished_file="build/tab100.tex",  vars_name=[r"$\text{Anzahl}$"], label_text="tab100", caption_text=r"Die Werte für den Druck in dem Glaszylinder, die Anzahl der Pulse und die Position des Maximums." , precision=1)
-#Generate table with calculated data
-some.tabelle([pulse3[0:19],pulse3[20:39],pulse3[40:59],pulse3[60:79],pulse3[80:99]], finished_file="build/tabc.tex", vars_name=[r"Pulse", r"Pulse", r"Pulse", r"Pulse", r"Pulse"], label_text="tabc", caption_text=r"Die Pulse wurden zur Analyse der Statistik des Radioaktiven Zerfalls bestimmt.", precision=1) 
+import scipy.constants as const
 
-#extra values
-p0 = 1013 
-x01 = 0.027
-x02 = 0.02
-#functions 
+plt.rcParams.update({'font.size': 22})
 
-x1 =  x01* p1/p0
-x2 =  x02* p2/p0
+I_vert = 2.28 *0.1
+B = const.mu_0 * 8 * 20 * I_vert /(np.sqrt(125)*0.11735)
 
-max1 = max1/1120 * 4e6
-max2 = max2/1099 * 4e6
+f, hor1, sweep1, hor2, sweep2 = np.genfromtxt("data/data.txt", unpack=True)
 
-Steigung1, yAbschnitt1, err1 = some.linReg(x=x1*1e3, y=pulse1, p=x1[16:19]*1e3, q=pulse1[16:19], x_name=r"$x_1 / \si{\milli\meter}$", y_name=r"$\frac{N}{\SI{120}{\second}}$", num=1,  x_add=1, file_name="build/plota.pdf")
-Steigung1b, yAbschnitt1b, err1b = some.linReg(x=x1*1e3, y=max1*1e-6, p=x1[:17]*1e3, q=max1[:17]*1e-6, x_name=r"$x_1 / \si{\milli\meter}$", y_name=r"$E / \si{\mega\electronvolt}$", num=2,  x_add=1, file_name="build/plotb.pdf")
+f= f*1000
+hor1 = (hor1-13.9)*0.3 
+hor2 = (hor2-13.9)*0.3 
+sweep1 = sweep1*0.1
+sweep2 = sweep2*0.1
 
-steigungerr1 = ufloat(Steigung1, err1)*1e3
-mitReichw1 = (1/2 * pulse1[0] - yAbschnitt1)/steigungerr1
-EnergiemitReichw1 = (mitReichw1*1e3/3.1)**(2/3)
+sweep1_B = const.mu_0 * 8 * 11 * sweep1/(np.sqrt(125)*0.1639)
+sweep2_B = const.mu_0 * 8 * 11 * sweep2/(np.sqrt(125)*0.1639)
 
-steig1 = ufloat(Steigung1, err1)
-E_R1 = mitReichw1*1000*Steigung1+yAbschnitt1
+hor1_B = const.mu_0 * 8 * 154 * hor1/(np.sqrt(125)*0.1579)
+hor2_B = const.mu_0 * 8 * 154 * hor2/(np.sqrt(125)*0.1579)
 
-Steigung2, yAbschnitt2, err2 = some.linReg(x=x2*1e3, y=pulse2, p=x2[5:18]*1e3, q=pulse2[5:18], x_name=r"$x_2 / \si{\milli\meter}$", y_name=r"$\frac{N}{\SI{120}{\second}}$", num=3,  x_add=1, file_name="build/plotc.pdf")
-Steigung2b, yAbschnitt2b, err2b = some.linReg(x=x2*1e3, y=max2*1e-6, p=x2[:]*1e3, q=max2[:]*1e-6, x_name=r"$x_2 / \si{\milli\meter}$", y_name=r"$E / \si{\mega\electronvolt}$", num=4,  x_add=1, file_name="build/plotd.pdf")
+B1 = sweep1_B + hor1_B 
+B2 = sweep2_B + hor2_B
 
-steigungerr2 = ufloat(Steigung2, err2)*1e3
-mitReichw2 = (1/2 * pulse2[0] - yAbschnitt2)/steigungerr2
-EnergiemitReichw2 = (mitReichw2*1e3/3.1)**(2/3)
+def func(x, m, n):
+    return m*x + n 
 
-steig2 = ufloat(Steigung2, err2)
-E_R2 = mitReichw2*1000*Steigung2+yAbschnitt2
+param1, cov1 = curve_fit(func, f, B1)
+err1 = np.sqrt(np.diag(cov1))
+a1 = ufloat(param1[0], err1[0])
+b1 = ufloat(param1[1], err1[1])
 
-some.tabelle([x1*1e3, pulse1, max1*1e-6], finished_file="build/tab1.tex", vars_name=[r"$x_1 / \si{\milli\meter}$", r"$\frac{N}{\SI{120}{\second}}$", r"$E /\si{\mega\electronvolt} $"], label_text="tab1", caption_text=r"Die Reichweite $x_1$, die Anzahl der Impulse und die Position des Maximums.", precision=2) 
-some.tabelle([x2*1e3, pulse2, max2*1e-6], finished_file="build/tab2.tex", vars_name=[r"$x_2 / \si{\milli\meter}$", r"$\frac{N}{\SI{120}{\second}}$", r"$E / \si{\mega\electronvolt}$"], label_text="tab2", caption_text=r"Die Reichweite $x_2$, die Anzahl der Impulse und die Position des Maximums.", precision=2) 
-#some.tabelle(, finished_file="tab<++>.tex", vars_name=[r"<++>", r"<++>"], label_text="tab<++>", caption_text=r"<++>", precision=2) 
-#Generate linReg-Plot
-#some.linReg(x=<++>, y=<++>, x_name=r"<++>", y_name=r"<++>", num=<++>,  x_add=<++>, file_name="build/plot<++>.pdf")
-#Generate curve-fit-Plot 
-#some.curvefit(x=<++>, y=<++>, num=<++>, x_add=<++>, function=<++>, x_name=r"<++>", y_name=r"<++>", file_name="build/plot<++>.pdf")
+gf1 = const.h/const.value("Bohr magneton") /a1
 
-#def gauß(x, mean, std):
-#    return 1/np.sqrt(4*np.pi*std**2) *np.exp(-(x-mean)**2/(2*std**2)) 
-pulse3 = (pulse3- np.min(pulse3))/100
-mean = pulse3.mean()
-std = pulse3.std()
-np.random.seed(42)
-gauß = np.random.normal(mean, std, 10000)
-poisson = np.random.poisson(mean, 10000)
+param2, cov2 = curve_fit(func, f, B2)
+err2 = np.sqrt(np.diag(cov2))
+a2 = ufloat(param2[0], err2[0])
+b2 = ufloat(param2[1], err2[1])
 
-plt.figure(5) 
-plt.hist(pulse3, bins=20, label="Daten", density=True)
-plt.hist(gauß, bins=20, label="Gauß", color="green", density= True, histtype='step')
-plt.ylabel("Wahrscheinlichkeit")
-plt.xlabel("Pulse")
+gf2 = const.h/const.value("Bohr magneton") /a2
+
+plt.figure(figsize=(15,8))
+plt.plot(f, B1, "x", label="Daten")
+plt.plot(f, B2, "x", label="Daten")
+plt.plot(f, func(f, *param1), label="Fit")
+plt.plot(f, func(f, *param2), label="Fit")
 plt.legend(loc="best")
-plt.tight_layout()
-plt.savefig("build/plotf.pdf") 
-plt.figure(6) 
-plt.hist(pulse3, bins=20, label="Daten", density=True)
-plt.hist(poisson, bins=20, label="Poisson", density = True, histtype='step')
-plt.ylabel("Wahrscheinlichkeit")
-plt.xlabel("Pulse")
-plt.legend(loc="best")
-plt.tight_layout()
-plt.savefig("build/plote.pdf") 
+plt.savefig("plots/fits.pdf") 
 
+f=  open("plots/results.txt", "w")
+f.write(f"B_Erdmagnetfeld: {B}\n")
+f.write(f"Peak 1: a1 = {a1}, b1 = {b1}\n")
+f.write(f"Peak 2: a2 = {a2}, b2 = {b2}\n")
+f.write(f"gf: {gf1}")
 
-#save solution
-file = open("build/solution.txt", "w")
-file.write(f"V701\n\nWerte aus Fit 1\nSteigung 1 = {Steigung1}+-{err1}\nyAbschnitt1 = {yAbschnitt1}\nmittlere Reichweite 1= {mitReichw1}\nEnergie bei mittlerer Reichweite = {EnergiemitReichw1}\ndE/dx = {Steigung1b}+-{err1b}\nyAbschnitt1b = {yAbschnitt1b}\n\nWerte aus Fit 2\nSteigung 2 = {Steigung2}+-{err2}\nyAbschnitt2 = {yAbschnitt2}\nmittlere Reichweite 2= {mitReichw2}\nEnergie bei mittlerer Reichweite = {EnergiemitReichw2}\ndE/dx = {Steigung2b}+-{err2b}\nyAbschnitt2b = {yAbschnitt2b}\n\nZählrate:\nMittelwert = {mean}\nStandardabweichung={std}\nVarianz=Std^2={std**2}\nSeeed = 42\n\n\nEnergie1: {E_R1}MeV\nEnergie2: {E_R2} MeV")
-file.close()
+f.close()
+
 
--- V46/plot.py
+++ V46/plot.py
@@ -6,6 +6,8 @@ from uncertainties import ufloat
 from scipy.optimize import curve_fit
 import scipy.constants as const
 
+plt.rcParams.update({'font.size': 22})
+
 lambda_, deg1_1, min1_1, deg1_2, min1_2,  deg2_1, min2_1, deg2_2, min2_2, deg3_1, min3_1, deg3_2, min3_2 = np.genfromtxt("data/data.txt", unpack=True)
 
 
@@ -17,35 +19,64 @@ deg2_2 = deg2_2 + min2_2/60
 deg3_1 = deg3_1 + min3_1/60
 deg3_2 = deg3_2 + min3_2/60
 
-winkel1 = 0.5*(deg1_1 + deg1_2)
-winkel2 = 0.5*(deg2_1 + deg2_2) 
-winkel3 = 0.5*(deg3_1 + deg3_2) 
+winkel1 = 0.5*(deg1_1 - deg1_2)
+winkel2 = 0.5*(deg2_1 - deg2_2) 
+winkel3 = 0.5*(deg3_1 - deg3_2) 
 
 winkel1 = np.deg2rad(winkel1)/5.11e-3
 winkel2 = np.deg2rad(winkel2)/1.296e-3
 winkel3 = np.deg2rad(winkel3)/1.36e-3
 
-diff2 = winkel2-winkel1 
-diff3 = winkel3-winkel1
+plt.figure()
+plt.plot(lambda_**2, winkel1, "x", label="Reine Probe")
+plt.plot(lambda_**2, winkel2, "x", label="Probe 1")
+plt.plot(lambda_**2, winkel3, "x", label="Probe 2")
+plt.legend(loc="best")
+plt.grid()
+plt.savefig("plots/AlleProben.pdf")
 
-N2 = 2.8e18
-N3 = 1.2e18
-n = 1
+diff1 = np.abs(winkel2-winkel1) 
+diff2 = np.abs(winkel3-winkel1)
+
+N1 = 2.8e18
+N2 = 1.2e18
+n = 3.57
 B = 392e-3
 
-a = const.elementary_charge**3 / (8* np.pi**2 *const.epsilon_0 * const.speed_of_light**3)
 
+def mass(x, a):
+    return a * x 
 
-def mass(lambda_, m):
-    return a * 1/m**2 * lambda_**2 
+params1, cov1 = curve_fit(mass, lambda_**2, diff1, p0=[12e12] )
+
+plt.figure()
+plt.plot(lambda_**2, diff1, "x", label="Daten")
+plt.plot(lambda_**2, mass(lambda_**2, *params1), label="Fit")
+plt.legend(loc="best")
+plt.grid()
+plt.savefig("plots/Probe1.pdf")
+
+err1 = np.diag(np.sqrt(cov1))
+a1 = ufloat(params1[0], err1[0])
+
+mass1 = unp.sqrt(const.elementary_charge**3 *N1 *B / (8* np.pi**2 *const.epsilon_0 * const.speed_of_light**3 * n * a1))
+
+params2, cov2 = curve_fit(mass, lambda_**2, diff2, p0=[12e12] )
 
 plt.figure()
 plt.plot(lambda_**2, diff2, "x", label="Daten")
+plt.plot(lambda_**2, mass(lambda_**2, *params2), label="Fit")
 plt.legend(loc="best")
-plt.savefig("plots/Probe1.pdf")
-
-plt.figure()
-plt.plot(lambda_**2, diff3, "x", label="Daten")
-plt.legend(loc="best")
+plt.grid()
 plt.savefig("plots/Probe2.pdf")
 
+err2 = np.diag(np.sqrt(cov2))
+a2 = ufloat(params2[0], err2[0])
+
+mass2 = unp.sqrt(const.elementary_charge**3 *N2 *B / (8* np.pi**2 *const.epsilon_0 * const.speed_of_light**3 * n * a2))
+
+print(mass2/const.electron_mass)
+
+f=  open("plots/results.txt", "w")
+f.write(f"Hallo")
+f.close()
